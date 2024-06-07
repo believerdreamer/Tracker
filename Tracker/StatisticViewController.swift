a11843c1cce04c553caa15a65d646d0c7f8a34ef
Arkhip Semenov <liteplay03@ya.ru>
2024-06-07 16:39:11 +0300

    initial comit

---
+++ Tracker/StatisticViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Архип Семёнов on 07.06.2024.
+//
+
+import UIKit
+
+class StatisticViewController: UIViewController { // MARK: - UIViewController
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
